397c5544fef1c1d8d15d72a9234e546c757b7ff9
Ben Lawson <blinternetgeek@gmail.com>
2017-09-22 12:43:34 -0700

    color coded pins, marker clusterer plus

--- csvToJSON.py
+++ csvToJSON.py
@@ -2,17 +2,53 @@
 
 import csv
 import json
+from datetime import datetime
+from dateutil import parser
 
-luxlist = []
-with open('mylux_all_users.csv', 'rb') as csvfile:
-    luxreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-    luxreader.next() # skip first line
-    for row in luxreader:
-        coords = row[2].split(',')
-        lat = float(coords[0])
-        lng = float(coords[1])
-        label = row[3]
-        if lat != 0.0 and lng != 0.0 and row[1].lower() != 'none':
-            lux = int(row[1])
-            luxlist.append({'lat': lat, 'lng': lng, 'lux': lux, 'time': row[4], 'lbl': label})
-print json.dumps(luxlist, ensure_ascii=False) # some foreign characters aren't ascii
+def parse_date(timestamp):
+    return parser.parse(timestamp)
+    # for fmt in ('%Y-%m-%d %H:%M:%S %z' ,'%Y-%m-%d %H:%M:%S %Z'): # utc offset, named timezone
+    #     try:
+    #         return datetime.strptime(timestamp, fmt)
+    #     except ValueError:
+    #         pass
+    #raise ValueError('No valid date format found for ', timestamp)
+
+def is_day_time(time):
+    hour = time.hour
+    return hour >= 6 and hour <= 18
+
+# return: string corresponding to color
+def color_for_lux_time(lux, timestamp):
+    color = 'r'
+    if is_day_time(parse_date(timestamp)):
+        if lux > 100: # bright during day
+            color = 'b' # blue
+        else: # dark during day
+            color = 'g' # gray
+    else: # night
+        if lux > 100:
+            color = 'y'
+        else:
+            color = 'r'
+    return color
+
+def main():
+    luxlist = []
+    with open('mylux_all_users.csv', 'rb') as csvfile:
+        luxreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        luxreader.next() # skip first line
+        for row in luxreader:
+            coords = row[2].split(',')
+            lat = float(coords[0])
+            lng = float(coords[1])
+            label = row[3]
+            timestamp = row[4]
+            if lat != 0.0 and lng != 0.0 and row[1].lower() != 'none':
+                 lux = int(row[1])
+                 color = color_for_lux_time(lux, timestamp)
+                 luxlist.append({'lat': lat, 'lng': lng, 'lx': lux, 'tm': timestamp, 'lb': label, 'cl': color})
+    print json.dumps(luxlist, ensure_ascii=False) # some foreign characters aren't ascii
+
+if __name__ == "__main__":
+    main()
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+function markerImageFromURL(url) {
+  return new google.maps.MarkerImage(url,
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34))
+}
+
 function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 3,
     center: {lat: 32.8873915, lng: -117.2453427}
   })
+  
+  var coloredPins = {
+    b: markerImageFromURL('img/blue-pin.png'),
+    g: markerImageFromURL('img/gray-pin.png'),
+    r: markerImageFromURL('img/red-pin.png'),
+    y: markerImageFromURL('img/yellow-pin.png')
+  }
 
   fetch('data.json').then(res => res.json())
     .then((data) => {
@@ -16,10 +30,11 @@ function initMap() {
   function initMarkers(locations) {
     var markers = locations.map(function(location, i) {
       var marker = new google.maps.Marker({
-        position: location
+        position: location,
+        icon: coloredPins[location.cl]
       })
       var iw =  new google.maps.InfoWindow({
-        content: `<b>${ location.lbl }</b><br>${ location.lux } lux <br> ${ location.time }`
+        content: `<b>${ location.lb }</b><br>${ location.lx } lux <br> ${ location.tm }`
       })
       marker.addListener('click', function() {
         iw.open(map, marker)
