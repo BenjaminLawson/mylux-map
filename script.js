e0aa612aea43561f96e8b6d5daf61c3e600f6328
Ben Lawson <blinternetgeek@gmail.com>
2017-09-22 13:31:12 -0700

    chunk loading

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-function markerImageFromURL(url) {
+function markerImageFromURL (url) {
   return new google.maps.MarkerImage(url,
         new google.maps.Size(21, 34),
         new google.maps.Point(0,0),
         new google.maps.Point(10, 34))
+}
+
+function markerFromLocation (loc) {
+  var coloredPins = {
+    b: markerImageFromURL('img/blue-pin.png'),
+    g: markerImageFromURL('img/gray-pin.png'),
+    r: markerImageFromURL('img/red-pin.png'),
+    y: markerImageFromURL('img/yellow-pin.png')
+  }
+  
+  var marker = new google.maps.Marker({
+    position: loc,
+    icon: coloredPins[loc.cl]
+  })
+  var iw =  new google.maps.InfoWindow({
+    content: `<b>${ loc.lb }</b><br>${ loc.lx } lux <br> ${ loc.tm }`
+  })
+  marker.addListener('click', function() {
+    iw.open(map, marker)
+  })
+
+  return marker
 }
 
 function initMap() {
@@ -11,45 +33,50 @@ function initMap() {
     zoom: 3,
     center: {lat: 32.8873915, lng: -117.2453427}
   })
-  
-  var coloredPins = {
-    b: markerImageFromURL('img/blue-pin.png'),
-    g: markerImageFromURL('img/gray-pin.png'),
-    r: markerImageFromURL('img/red-pin.png'),
-    y: markerImageFromURL('img/yellow-pin.png')
-  }
 
   fetch('data.json').then(res => res.json())
     .then((data) => {
-      console.log(data)
       initMarkers(data)
     }).catch(err => {
       console.log('could not fetch data.json: ', err)
     })
 
-  function initMarkers(locations) {
-    var markers = locations.map(function(location, i) {
-      var marker = new google.maps.Marker({
-        position: location,
-        icon: coloredPins[location.cl]
-      })
-      var iw =  new google.maps.InfoWindow({
-        content: `<b>${ location.lb }</b><br>${ location.lx } lux <br> ${ location.tm }`
-      })
-      marker.addListener('click', function() {
-        iw.open(map, marker)
-      })
-      return marker
-    })
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(
+    map,
+    [],
+    {
+      maxZoom: 17,
+      imagePath: 'img/m'
+    }
+  )
 
-    // Add a marker clusterer to manage the markers.
-    var markerCluster = new MarkerClusterer(
-      map,
-      markers,
-      {
-        maxZoom: 17,
-        imagePath: 'img/m'
+  function initMarkers(locations) {
+    var chunkSize = 250
+    var index = 0
+    processChunk()
+    function processChunk() {
+      for (var i = 0; i < chunkSize && index < locations.length; i++) {
+        markerCluster.addMarker(markerFromLocation(locations[index]))
+        index++
       }
-    )
+      
+      if (index < locations.length) {
+        setTimeout(processChunk, 90)
+      }
+      else {
+        console.log('Done processing data')
+      }
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
